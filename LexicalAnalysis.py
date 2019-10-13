1cac643cd81736236f35bd9274b7e00df8e27365
ryoyen1 <43334112+ryoyen1@users.noreply.github.com>
2019-10-13 21:37:34 +0700

    Add files via upload

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -1,36 +1,63 @@
 from sly import Lexer
 
 class simpleLexer(Lexer):
-    tokens = { STRING, NAME, CLASS , NUMBER , PLUS , MINUS, DIVIDE, MULTI, EQUAL, LPAREN, RPAREN}
-    ignore = '\t'
+    tokens = { PHP, ECHO, FUNCTION, IF, STRING,
+               NAME, CLASS , NUMBER , PLUS , MINUS,
+               DIVIDE, MULTI, EQUAL, LPAREN, RPAREN}
+    ignore = ' \t'
 
     #Operators
-    symbol = { '=', '+', '-', '/', '*', '(', ')', '{' ,'}', ',', ';'}
+    symbol = {'(', ')', '{' ,'}', ',', ';'}
 
     #Tokens
+    # NUMBER = r'\d+'
+    PLUS = r'\+'
+    MINUS = r'-'
+    MULTI = r'\*'
+    DIVIDE = r'/'
+    EQUAL = r'='
+
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    CLASS = r'CLASS'
+    NAME['class'] = CLASS
+    NAME['if'] = IF
+    NAME['function'] = FUNCTION
+    NAME['echo'] = ECHO
+    # NAME['<?php'] = PHP
+
     STRING = r'\".*?\"'
 
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
 
-column = 0
-line = 1
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
 
-data = '''<?php
-          class MyClass {
-            function abc(){ $i=5;
-            $z=$i*2;
-            echo "One '$=".$z;}
-          }
-          ?>'''
 
-space = " "
-lexeme = ''
+# data = '''<?php
+#           class MyClass {
+#             function abc(){ $i=5;
+#             $z=$i*2;
+#             echo "One '$=".$z;}
+#           }
+#           ?>'''
 
-@_('r\d+')
-def NUMBER(self, t):
-    t.value = int(t.value)
-    return t
+data = '''
+ryo
+is
+while
+test
+if
+echo'''
+
+lexer = simpleLexer()
+for tok in lexer.tokenize(data):
+    print(tok)
+
+
+
 
 
 # for i, char in enumerate(data):
@@ -42,4 +69,4 @@ def NUMBER(self, t):
 #                 print(lexeme.replace('\n','<newline>'))
 #                 lexeme = ""
 
-print(lexeme)
+
